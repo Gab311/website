219378070bf12af532b2a3b5ad77171cd1215904
Gab <gabmainacc1@gmail.com>
2022-09-08 13:33:50 +0800

    scrape meeting links

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
+import webbrowser
+import re
+
+url = "https://pastebin.com/psRsW3fL"
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "html.parser")
+meet = soup.find_all("div", class_="de1")
+
+#remove html tags
+meet = str(meet)
+meet = re.sub('<[^>]*>', '', meet)
+meet = meet.replace('[', '')
+meet = meet.replace(']', '')
+meet = meet.replace(' ', '')
 
 
 st.title("Meeting Links")
-st.button("Join Meeting")
+if st.button("Join Meeting"):
+    webbrowser.open(f"{meet}")
 
 
 hide_streamlit_style = """
